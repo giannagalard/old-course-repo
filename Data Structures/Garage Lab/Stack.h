e3feda60facb3781e1d8e20a346cc5f416bc4f6d
Gianna Galard <80483712+giannagalard@users.noreply.github.com>
2021-03-26 18:34:57 -0400

    Update Stack.h

--- Data Structures/Garage Lab/Stack.h
+++ Data Structures/Garage Lab/Stack.h
@@ -14,27 +14,28 @@ struct Cars //Struct class implementation
     Cars() { moved = 0; }
 };
 
-template<class StackType>
-class Stack {
+template <class StackType>
+class Stack
+{
 
 public:
     Stack(int MaxStackSize = 10);
     ~Stack() { delete[] stack; }
     bool IsEmpty() const { return top == -1; }
     bool IsFull() const { return top == MaxTop; } //checks if stack is full
-    StackType Top() const; // returns full struct from top
+    StackType Top() const;                        // returns full struct from top
     void push(StackType x);
     void pop();
 
     bool search(Cars x); //Returns the index of the plate
 
 private:
-    int top; // current top of stack
-    int MaxTop; // max value for top
-    StackType* stack; // element array
+    int top;          // current top of stack
+    int MaxTop;       // max value for top
+    StackType *stack; // element array
 };
 
-template<class StackType>
+template <class StackType>
 Stack<StackType>::Stack(int MaxStackSize)
 {
     //Pre: none
@@ -45,7 +46,7 @@ Stack<StackType>::Stack(int MaxStackSize)
     top = -1;
 }
 
-template<class StackType>
+template <class StackType>
 StackType Stack<StackType>::Top() const
 {
     //Pre: stack is not empty
@@ -54,21 +55,23 @@ StackType Stack<StackType>::Top() const
         throw logic_error("Top fails: Stack is empty"); // Top Fails
     return stack[top];
 }
-template<class StackType>
+template <class StackType>
 void Stack<StackType>::push(StackType x)
 {
     //Pre: Stack is not full
     //Post: Push x to stack
     // Stack has one more element
-    if (IsFull()) throw logic_error("Push fails: full stack"); // Push Fails
+    if (IsFull())
+        throw logic_error("Push fails: full stack"); // Push Fails
     stack[++top] = x;
 }
-template<class StackType>
+template <class StackType>
 void Stack<StackType>::pop()
 {
     //Pre: Stack is not empty
     //Post: Stack has one less element
-    if (IsEmpty()) {
+    if (IsEmpty())
+    {
         throw logic_error("Pop fails: Stack is empty");
         exit(1);
     }; //pop fails
@@ -78,7 +81,7 @@ void Stack<StackType>::pop()
 //Pre: Accepts a string
 //Post: Returns the index if string is located
 
-template<class StackType> //looks to see if the stack is full
+template <class StackType> //looks to see if the stack is full
 bool Stack<StackType>::search(Cars x)
 {
     if (top != -1)
@@ -86,7 +89,6 @@ bool Stack<StackType>::search(Cars x)
         {
             if (stack[i].plate == x.plate)
                 return true;
-
         }
     return false;
 }
