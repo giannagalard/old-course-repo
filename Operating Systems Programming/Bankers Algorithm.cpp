553b8113dd41283ac49bc1d3ec324e3695100221
Gianna Galard <80483712+giannagalard@users.noreply.github.com>
2021-04-08 22:27:48 -0400

    Update Bankers Algorithm.cpp

--- Operating Systems Programming/Bankers Algorithm.cpp
+++ Operating Systems Programming/Bankers Algorithm.cpp
@@ -11,3 +11,18 @@ int main()
 
     // available matrix
     int available[3] = {1, 5, 3};
+    
+    // max matrix
+    int max[5][3] = {{9, 1, 7},  // P0
+                     {5, 3, 5},  // P1
+                     {0, 2, 9},  // P2
+                     {7, 7, 7},  // P3
+                     {1, 3, 3}}; // P4
+
+    // allocation matrix
+    int allocation[5][3] = {{1, 6, 1},  // P0
+                            {0, 0, 2},  // P1
+                            {5, 0, 3},  // P2
+                            {8, 7, 7},  // P3
+                            {0, 0, 3}}; // P4
+
