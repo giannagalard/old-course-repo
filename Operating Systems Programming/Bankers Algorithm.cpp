55e1d623c832165bc7c440132867888194dba836
Gianna Galard <80483712+giannagalard@users.noreply.github.com>
2021-04-08 22:25:45 -0400

    Create Bankers Algorithm.cpp

---
+++ Operating Systems Programming/Bankers Algorithm.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int processNum, resources;
+    processNum = 5; // P0, P1, P2, P3, P4
+    resources = 3;
+
+    // available matrix
+    int available[3] = {1, 5, 3};
