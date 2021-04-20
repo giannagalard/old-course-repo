ab91fdc5610dd3c1458abdf818da03b54b26f6ed
Gianna Galard <80483712+giannagalard@users.noreply.github.com>
2021-04-20 04:42:18 -0400

    Update Clickprogram.java

--- Object-Oriented Software Design/Group Project/Clickprogram.java
+++ Object-Oriented Software Design/Group Project/Clickprogram.java
@@ -4,33 +4,33 @@ import javax.swing.*;
 import java.awt.event.*;
 
 public class Clickprogram extends JFrame {
-
-  public static void main(String[] args) {
-    new Clickprogram();
-  }
-
-  private JButton button1;
-
-  public Clickprogram() {
-    this.setSize(300, 150);
-    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    this.setTitle("Click Event");
-
-    Clicklistener click = new Clicklistener();
-
-    JPanel panel1 = new JPanel();
-    button1 = new JButton("Click Me!");
-    button1.addActionListener(click);
-    panel1.add(button1);
-    this.add(panel1);
-    this.setVisible(true);
-  }
-
-  private class Clicklistener implements ActionListener {
-    public void actionPerformed(ActionEvent e) {
-      if (e.getSource() == button1) {
-        button1.setText("The button has been clicked");
-      }
+	
+	public static void main(String[] args) {
+		new Clickprogram();
+	}
+	
+	private JButton button1;
+    
+    public Clickprogram() {   
+    	this.setSize(300,150);
+    	this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    	this.setTitle("Click Event");
+        
+    	Clicklistener click= new Clicklistener();
+        
+    	JPanel panel1= new JPanel();
+    	button1= new JButton ("Click Me!");
+    	button1.addActionListener(click);
+    	panel1.add(button1);
+    	this.add(panel1);
+    	this.setVisible(true);
     }
-  }
+    
+    private class Clicklistener implements ActionListener {
+    	public void actionPerformed(ActionEvent e) {
+    		if (e.getSource() == button1) {
+    			button1.setText("The button has been clicked");
+    		}
+    	}
+    }   
 }
