2eba093ccfd161631533eed5e6d2e6d7e6c929a5
Gianna Galard <80483712+giannagalard@users.noreply.github.com>
2021-04-20 04:43:00 -0400

    Update Search.java

--- Object-Oriented Software Design/Group Project/Search.java
+++ Object-Oriented Software Design/Group Project/Search.java
@@ -5,19 +5,19 @@ import java.util.List;
 import javax.swing.JOptionPane;
 
 public class Search {
+	
+	public Search() {
+		System.out.println("What would you like to look for today?");
+		
+		List<StateData> listOfStates = AccessibleStateDataAPI.findStateDataByAddress("forest ave");
+		StateData state = listOfStates.get(0);
+		JOptionPane.showMessageDialog(null,
+				"Address: " + state.getAddress() + " " + state.getAddress() + ", City: " + state.getCity()
+						+ ", State: " + state.getState() + ", Zipcode: " + state.getZipcode());
+		
+	}
 
-  public Search() {
-    System.out.println("What would you like to look for today?");
-
-    List < StateData > listOfStates = AccessibleStateDataAPI.findStateDataByStreet("forest ave");
-    StateData state = listOfStates.get(0);
-    JOptionPane.showMessageDialog(null,
-      "Address: " + state.getStreetNum() + " " + state.getStreet() + ", City: " + state.getCity() +
-      ", State: " + state.getState() + ", Zipcode: " + state.getZipcode());
-
-  }
-
-  public static void main(String[] args) {
-    new Search();
-  }
+	public static void main(String[] args) {
+		new Search();
+	}
 }
