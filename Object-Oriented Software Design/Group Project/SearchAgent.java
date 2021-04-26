ffcfcb281f8826d5383d56ec5b6a8a3edcb91668
Gianna Galard <80483712+giannagalard@users.noreply.github.com>
2021-04-26 13:56:48 -0400

    Update SearchAgent.java

--- Object-Oriented Software Design/Group Project/SearchAgent.java
+++ Object-Oriented Software Design/Group Project/SearchAgent.java
@@ -2,64 +2,90 @@ package realty;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.util.List;
+
 import javax.swing.*;
 
-public class Login extends JFrame implements ActionListener {
+public class SearchAgent extends JFrame{
+
 	JPanel panel;
-	JLabel username, password, text;
-	JTextField usernameText;
-	JPasswordField passwordText;
-	JButton submit, cancel;
-	boolean isLoggedIn = false;
+	JLabel firstName, lastName, text;
+	JButton firstNameButton, lastNameButton;
+	JTextField firstNameText, lastNameText;
+	
+	public SearchAgent() {	
+		firstName = new JLabel();
+		firstName.setText("     First Name:"); // (must be 5 spaces )
+		firstNameText = new JTextField();
+		lastName = new JLabel();
+		lastName.setText("     Last Name:"); // (must be 5 spaces )
+		lastNameText = new JTextField();
 
-	Login() {
-		username = new JLabel();
-		username.setText("     Username:"); // (must be 5 spaces )
-		usernameText = new JTextField();
-		password = new JLabel();
-		password.setText("     Password:"); // (must be 5 spaces )
-		passwordText = new JPasswordField();
-		submit = new JButton("Submit"); // text for submit button
-		
-		submit.addActionListener(this);
+		firstNameButton = new JButton("Search");
+		lastNameButton = new JButton("Search");
 
-		panel = new JPanel(new GridLayout(3, 1));
-		panel.add(username); // user label
-		panel.add(usernameText); // user input
-		panel.add(password); // pass label
-		panel.add(passwordText); // user input
+		panel = new JPanel(new GridLayout(2, 1));
+		panel.add(firstName);
+		panel.add(firstNameText);
+		panel.add(firstNameButton);
+		panel.add(lastName);
+		panel.add(lastNameText);
+		panel.add(lastNameButton);
 		text = new JLabel();
-		panel.add(text); // blank box to put submit button under user input
-		panel.add(submit); // submit button
 		
-		add(panel, BorderLayout.CENTER);
-		setTitle("Please Login:");
-		setSize(375, 150); // setting the size of the window
+		ClickListener click = new ClickListener();	
+		firstNameButton.addActionListener(click);
+		lastNameButton.addActionListener(click);
+
+		setTitle("Search for agents:");
+		setSize(300, 100); // setting the size of the window
 		setVisible(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		add(panel, BorderLayout.CENTER);
 	}
-
+	
+	public class ClickListener implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			String firstName = firstNameText.getText();
+			String lastName = lastNameText.getText();
+			
+			if(e.getSource() == firstNameButton) {
+				List<AgentData> listOfAgents = AccessibleAgentDataAPI.findAgentDataByFirstName(firstName);
+				int i = 1;
+				String values = "";
+				for(AgentData agent: listOfAgents) {
+					values += "Agent " + i + ": " + agent.getFirstName() + " " + agent.getLastName() + "\n";
+					i++;
+				}
+				if (values == "") {
+					JOptionPane.showMessageDialog(null, "Not Found!");
+				}
+				else {
+					JOptionPane.showMessageDialog(null, values);
+				}
+			}
+			else if(e.getSource() == lastNameButton) {
+				List<AgentData> listOfAgents = AccessibleAgentDataAPI.findAgentDataByLastName(lastName);
+				int i = 1;
+				String values = "";
+				for(AgentData agent: listOfAgents) {
+					values += "Agent " + i + ": " + agent.getFirstName() + " " + agent.getLastName() + "\n";
+					i++;
+				}
+				if (values == "") {
+					JOptionPane.showMessageDialog(null, "Not Found!");
+				}
+				else {
+					JOptionPane.showMessageDialog(null, values);
+				}
+			}
+		}
+	}
+	
 	public static void main(String[] args) {
-		new Login();
-	}
-
-	@Override // this is the login / register ( basically they can either login with admin
-				// credentials or make a new account and login instantly lol )
-	public void actionPerformed(ActionEvent ae) {
-		String username = usernameText.getText();
-		String password = passwordText.getText();
-		if (username.trim().equals("admin") && password.trim().equals("admin")) {
-			setVisible(false);
-			System.out.println("Welcome back " + username + "!");
-			new Choice();
-		}
-		else if (username == "" || password == "") {
-			System.out.println("Missing Value!");
-		}
-		else {
-			setVisible(false);
-			System.out.println("Welcome back " + usernameText.getText() + "!");
-			new Choice();
-		}
-	}
+		new SearchAgent();
+ 	}
+	
 }
+
